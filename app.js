6bf930b80082dffffcada3de9310124f2e5aac39
Sajji <sajji.h@gmail.com>
2022-04-06 19:52:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var data = require ("./defense.json");
 var request = require('request');
 const fsPromises = fs.promises;
 
+var f2 = require("./postimportapi");
+
 app.listen(3001, () => {
   console.log("Application started and Listening on port 3001");
 });
@@ -21,12 +23,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+
 app.post("/", (req, res) => {
   var subName = req.body.yourname
  // var subEmail = req.body.youremail;
  res.send("DONE!");
 // fs.writeFileSync('./test.txt', subName);
-
+// async function craftFile() {
 var communityDomain = (
   `[
     {
@@ -81,8 +84,6 @@ dataset.{
 }
 `);
 
-
-
 var result = expression.evaluate(data);
 //console.log(result);
 
@@ -96,36 +97,38 @@ var altogetherNow = (
 );
 
 let finalz = altogetherNow.replace(",{",",");
+async function main() {
+  await (fs.writeFileSync('./fixed.json', finalz));
+  await (f2);
 
-//fs.writeFileSync('./DEFENSEImport.json', altogetherNow);
-async function jerk() {
 
-  fs.writeFileSync('./fixed.json', finalz);
 
 }
-jerk();
+main()
 
-});
+// function f2() {
+// var options = {
+//   'method': 'POST',
+//   'url': 'http://localhost:4400/rest/2.0/import/json-job',
+//   'headers': {
+//     'Authorization': 'Basic QWRtaW46YWRtaW4=',
+//     'Cookie': 'JSESSIONID=62be2896-c060-4adc-8b24-f66b1a15c806'
+//   },
+//   formData: {
+//     'file': {
+//       'value': fs.createReadStream('fixed.json'),
+//       'options': {
+//         'filename': '/fixed.json',
+//         'contentType': null
+//       }
+//     }
+//   }
+// };
+// request(options, function (error, response) {
+//   if (error) throw new Error(error);
+//   console.log(response.body);
+// });
+// }
 
-
-var options = {
-  'method': 'POST',
-  'url': 'http://localhost:4400/rest/2.0/import/json-job',
-  'headers': {
-    'Authorization': 'Basic QWRtaW46YWRtaW4=',
-    'Cookie': 'JSESSIONID=62be2896-c060-4adc-8b24-f66b1a15c806'
-  },
-  formData: {
-    'file': {
-      'value': fs.createReadStream('fixed.json'),
-      'options': {
-        'filename': '/fixed.json',
-        'contentType': null
-      }
-    }
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+}
+)
